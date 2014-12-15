22ef100796b044f1d7d41be70590b43f8d0c34c8
ki7mt <ki7mt@yahoo.com>
2014-12-15 10:40:03 +0000

    Update WSJT and WSPR help screens

--- scripts/help/pyenv-help-wsjt.cmd
+++ scripts/help/pyenv-help-wsjt.cmd
@@ -1,27 +1,21 @@
 ﻿@ECHO off
 CLS
 ECHO. -----------------------------------------------------------------
-ECHO.  Help Build WSPR
+ECHO.  Help Build WSJT
 ECHO. -----------------------------------------------------------------
 ECHO.
-ECHO   Usage: build-wspr ^( target ^)
-ECHO
-ECHO   There are several targets available for WSPR, the main being
-ECHO   [ install ] or [ package]. After checkout-wspr, use one of the
+ECHO   Usage: build-wsjt ^( target ^)
+ECHO.
+ECHO   There are several targets available for WSJT, the main being
+ECHO   [ install ] or [ package]. After checkout-wsjt, use one of the
 ECHO   following:
 ECHO.
-ECHO   libwspr.a  ..... WPSR Library
-ECHO   fmtest.exe ..... FMTest App
-ECHO   fmtave.exe ..... Ave app for FMTest
-ECHO   fcal.exe ....... Cal app for FMTest
-ECHO   fmeasure.exe ... Measure app for FMTest
-ECHO   gmtime2.o ...... Compile gmtime2.c
-ECHO   sound.o ........ Compile sound.c
-ECHO   WSPRcode.exe ... WSPR code testing App
-ECHO   wspr0.exe  ..... Command Line WSPR
-ECHO   WsprMod/w.pyd .. Audio Library for WSPR
-ECHO   install ........ Build and Install WSPR
-ECHO   package ........ Build Win32 Installer
+ECHO   libjt.a  ........... WSJT Library
+ECHO   jt65code.exe ....... JT65 code test app
+ECHO   jt4code.exe ........ JT4 code test app
+ECHO   WsjtMod/Audio.pyd .. Audio Library for WSJT
+ECHO   install ............ Build and Install WSJT
+ECHO   package ............ Build Win32 Installer
 ECHO. 
 ECHO   ^* Return to Main-Menu, type .. main-menu 
 GOTO EOF
