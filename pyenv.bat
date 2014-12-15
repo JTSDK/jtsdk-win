2008ba8c238c606a36df2afc4b7ada6ccf74f80a
ki7mt <ki7mt@yahoo.com>
2014-12-15 10:34:20 +0000

    Update pyenv

--- pyenv.bat
+++ pyenv.bat
@@ -23,44 +23,65 @@
 :: along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ::-----------------------------------------------------------------------------::
 
+:: Windows commands and Variables are in CAPS
+:: User Defined variables are lower case like %based%
+
 :: ENVIRONMENT
 @ECHO OFF
+ECHO Setting Up JTSDK-PY v2 Environment variables ...
 TITLE JTSDK Python Development Environment
 SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+SET PROMPT=$CJTSDK-PY$F $P$S
 SET LANG=en_US
 COLOR 0A
 
 :: PATH VARIABLES
+SET based=C:\JTSDK
+SET tools=%based%\tools\bin
+SET mgw=%based%\mingw32\bin
+SET inno=%based%\inno5
+SET scr=%based%\scripts
+SET pythonpath=%based%\Python33;%based%\Python33\Scripts;%based%\Python33\DLLs
+SET svnd=%based%\subversion\bin
 SET LIBRARY_PATH=
-SET BASED=C:\JTSDK
-SET TOOLS=%BASED%\tools\bin
-SET MGW=%BASED%\mingw32\bin
-SET INNO=%BASED%\inno5
-SET SCR=%BASED%\scripts
-SET PYP=%BASED%\Python33
-SET PYS=%BASED%\Python33\Scripts
-SET PYD=%BASED%\Python33\DLLs
-SET SVND=%BASED%\subversion\bin
-SET PATH=%BASED%;%MGW%;%PYP%;%PYS%;%PYD%;%TOOLS%;%INNO%;%SCR%;%SVND%;%WINDIR%\System32
-CD /D %BASED%
+SET PATH=%based%;%MGW%;%pythonpath%;%tools%;%innno%;%scr%;%svnd%;%WINDIR%\System32
+CD /D %based%
 
-:: GENERAL USE DOSKEY COMMANDS
-DOSKEY checkout="%SCR%\pyenv-co.bat" $1
-DOSKEY build="%SCR%\pyenv-build.bat" $1 $2
-DOSKEY env-info=CALL %SCR%\pyenv-info.bat
+:: CHECKOUT AND BUILD COMMANDS ( users *should not* edit these )
+DOSKEY checkout="%scr%\pyenv-checkout.cmd" $1
+DOSKEY build-wsjt="%scr%\pyenv-build-wsjt.cmd" $1
+DOSKEY build-wspr="%scr%\pyenv-build-wspr.cmd" $1
 DOSKEY make=C:\JTSDK\mingw32\bin\mingw32-make $*
 
-:: SVN POWER-USER COMMANDS
+:: HELP PAGES ( users *should not* edit these )
+DOSKEY main-menu=CD ^/D %based% ^&CALL %scr%\pyenv-info.cmd
+DOSKEY help-pyenv=CALL %based%\scripts\help\pyenv-help-main.cmd
+DOSKEY help-checkout=CALL %based%\scripts\help\pyenv-help-checkout.cmd
+DOSKEY help-wspr=CALL %based%\scripts\help\pyenv-help-wspr.cmd
+DOSKEY help-wsjt=CALL %based%\scripts\help\pyenv-help-wsjt.cmd
+
+:: USER DEFINABLE ALIAS COMMANDS ( use edit-alias to edit list )
+:: Note, this file gets overwritten with SVN update changes
+DOSKEY clear=CLS
 DOSKEY ss="svn.exe" $* status
 DOSKEY sv="svn.exe" $* status ^|grep "?"
 DOSKEY sa="svn.exe" $* status ^|grep "A"
 DOSKEY sm="svn.exe" $* status ^|grep "M"
 DOSKEY sd="svn.exe" $* status ^|grep "D"
 DOSKEY log="svn.exe" log -l $*
+DOSKEY logr="svn.exe" log -r $*
 DOSKEY logv="svn.exe" log -v -l $*
+DOSKEY logvr="svn.exe" log -v -r $*
+DOSKEY edit="%tools%\Sc351.exe" $1
 
-CALL %SCR%\pyenv-info.bat
-IF NOT EXIST %BASED%\src\NUL mkdir %BASED%\src
+:: RE-SET FILE ASSOCIATIONS
+SET PATHEXT=.COM;.EXE;.BAT;.CMD;.VBS;.VBE;.JS;.JSE;.WSF;.WSH;.MSC;.py
+assoc .py=Python.File > NUL
+assoc .pyc=Python.File > NUL
+ftype Python.File=%based%\Python33\python.exe "%1" %* > NUL
+
+:: RUN VERSION INFORMATION SCRIPT
+CALL %scr%\pyenv-info.cmd
 GOTO EOF
 
 :: LAUNCH CMD WINDOW
