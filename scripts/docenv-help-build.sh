ecda603818dc5df3d27bad60347b3616983a57d2
ki7mt <ki7mt@yahoo.com>
2015-06-08 16:59:00 +0000

    Set EOL to UNIX/OSX format

--- scripts/docenv-help-build.sh
+++ scripts/docenv-help-build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash.exe
 #
-# Title ........: docenv-help-build.sh
+# Title ........: docenv-help-co.sh
 # Project ......: Part of the JTSDK v2.0.0 Project
-# Description ..: Help File For Building WSJT Documentation
+# Description ..: Help File For Checking Out WSJT Documentation
 # Project URL ..: http://sourceforge.net/projects/wsjt/
-# Usage ........: From Within JTSDK-DOC, type: build-help
+# Usage ........: From Within JTSDK-DOC, type: co-help
 #
 # Author .......: Greg, Beam, KI7MT, <ki7mt@yahoo.com>
 # Copyright ....: Copyright (C) 2014-2015 Joe Taylor, K1JT
 # License ......: GPL-3
 #
-# docenv-help-build.sh is free software: you can redistribute it
+# docenv-help-co.sh is free software: you can redistribute it
 # and/or modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation either version 3 of the
 # License, or (at your option) any later version. 
 #
-# docenv-help-build.sh is distributed in the hope that it will be
+# docenv-help-co.sh is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
@@ -25,35 +25,30 @@
 #--------------------------------------------------------------------#
 
 # Exit on Errors
-set -e
+#set -e
 
 # Source Color Variables
 source /scripts/color-variables
 
+# Display Document Help Help Message
 clear
-echo -e ${C_Y}"JTSDK-DOC BUILD HELP PAGE\n"${C_NC}
-echo 'USAGE: [ build-doc.sh ] [ option ]'
+echo -e ${C_Y}"JTSDK-DOC CHECKOUT HELP PAGE"${C_NC}
+echo '  In order to build WSJT Documentation, you'
+echo '  must first perform a checkout from'
+echo '  WSJT @ SourceForge'
 echo ''
-echo 'OPTION(s): all map65 simjt wsjt wsjtx'
-echo '           wspr wsprx wfmt devg qref help clean'
+echo -e ${C_C}"ANONYMOUS CHECKOUT"${C_NC}
+echo '  Type: checkout-doc'
 echo ''
-echo -e ${C_C}"BUILD LINKED"${C_NC}
-echo '  All .....: ./build-doc.sh all'
-echo '  WSJT-X ..: ./build-doc.sh wsjtx'
-echo
-echo -e ${C_C}"BUILD DATA-URI - ( single-file )"${C_NC}
-echo '  All .....: ./build-doc.sh dall'
-echo '  WSJT-X ..: ./build-doc.sh dwsjtx'
-echo
-echo -e ${C_C}"CLEAN FILES & FOLDERS"${C_NC}
-echo '  All .....: ./build-doc.sh clean'
+echo -e ${C_C}"DEVELOPER CHECKOUT"${C_NC}
+echo "  cd $HOME"
+echo "  svn co https://$USER@svn.code.sf.net/p/wsjt/wsjt/branches/doc"
 echo ''
-echo -e ${C_C}"NOTE(s)"${C_NC}
-echo -e '  [1] The same method is used for all documentaion.
-      The prefix "d" designates a DATA-URI ( single-file )
-      version of the document.
-  
-  [2] To see additional Short-Cuts, type: lista'
+echo -e "  Replace [ ${C_Y}$USER${C_NC} ] with your SorceForge User Name."
+echo ''
+echo -e ${C_C}"BUILD COMMANDS"${C_NC}
+echo '  To List build commands, type ..: help-build'
+echo '  To List Short-Cuts, type ......: lista'
 echo ''
 
 exit 0
